50e690861315d6a64c86377d63b2b0ce3f98b444
zhouquan <zhouquan@chexiang.com>
2017-10-13 16:29:55 +0800

    format

--- demo-brand-service/src/main/java/demo/brand/service/server/NettyServerHandler.java
+++ demo-brand-service/src/main/java/demo/brand/service/server/NettyServerHandler.java
@@ -27,26 +27,26 @@ import io.netty.channel.ChannelHandlerContext;
  */
 public class NettyServerHandler extends ChannelHandlerAdapter {
 
-    @Override
-    public void channelRead(ChannelHandlerContext ctx, Object msg)
-	    throws Exception {
-	ByteBuf buf = (ByteBuf) msg;
-	byte[] req = new byte[buf.readableBytes()];
-	buf.readBytes(req);
-	String body = new String(req, "UTF-8");
-	//调用服务
-	String response = ServiceManager.doService(body);
-	ByteBuf resp = Unpooled.copiedBuffer(response.getBytes());
-	ctx.write(resp);
-    }
+	@Override
+	public void channelRead(ChannelHandlerContext ctx, Object msg)
+			throws Exception {
+		ByteBuf buf = (ByteBuf) msg;
+		byte[] req = new byte[buf.readableBytes()];
+		buf.readBytes(req);
+		String body = new String(req, "UTF-8");
+		// 调用服务
+		String response = ServiceManager.doService(body);
+		ByteBuf resp = Unpooled.copiedBuffer(response.getBytes());
+		ctx.write(resp);
+	}
 
-    @Override
-    public void channelReadComplete(ChannelHandlerContext ctx) throws Exception {
-	ctx.flush();
-    }
+	@Override
+	public void channelReadComplete(ChannelHandlerContext ctx) throws Exception {
+		ctx.flush();
+	}
 
-    @Override
-    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
-	ctx.close();
-    }
+	@Override
+	public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
+		ctx.close();
+	}
 }
